c9af29375cd9d90f9f7b99f875d4f414891f7ac0
Juan Martin Botero <jbotero@hotmail.com>
2020-09-11 11:19:04 -0400

    initial load

---
+++ Euler/Number.cs
@@ -0,0 +1,34 @@
+﻿using JuanMartin.Kernel.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuanMartin.Utilities.Euler
+{
+    public class Number : IComparable<Number>
+    {
+        public int Value { get; set; }
+        public string FrequencyMap { get; private set; }
+        public List<string> Patterns { get; private set; }
+        public List<int> Family { get; private set; }
+
+        public Number(int n)
+        {
+            Value = n;
+            FrequencyMap = UtilityString.GetCharacterFrequencyMap(n.ToString());
+            Patterns = UtilityMath.GetNumericPatterns(n);
+        }
+
+        public override string ToString()
+        {
+            return FrequencyMap;
+        }
+
+        public int CompareTo(Number obj)
+        {
+            return Value.CompareTo(obj.Value);
+        }
+    }
+}
---
+++ Tools/TrigramModel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JuanMartin.Utilities.Tools
+{
+    public class TrigramModel
+    {
+        Trigram[] trigrams;
+        public Trigram[] Trigrams
+        {
+            get { return trigrams; }
+        }
+
+        public TrigramModel(Hashtable trigramsAndCounts)
+        {
+            var keys2 = new List<string>();
+            var scores2 = new List<int>();
+            //convert hashtable to arrays
+            foreach (string key in trigramsAndCounts.Keys)
+            {
+                keys2.Add(key);
+                scores2.Add((int)trigramsAndCounts[key]);
+            }
+
+            var keys = keys2.ToArray();
+            var scores = scores2.ToArray();
+
+            // sort array results
+            Array.Sort(scores, keys);
+            Array.Reverse(keys);
+            Array.Reverse(scores);
+
+            //build final array
+            var result = new List<Trigram>();
+            for (int x = 0; x < keys.Length; x++)
+            {
+                result.Add(new Trigram(keys[x], scores[x]));
+            }
+
+            trigrams = result.ToArray();
+
+        }
+
+        public TrigramModel(string[] tgrams)
+        {
+            var keys2 = new List<string>();
+            var scores2 = new List<int>();
+            //convert hashtable to arrays
+            int score = 0;
+            foreach (string key in tgrams)
+            {
+                keys2.Add(key);
+                scores2.Add(score++);
+            }
+
+            var keys = keys2.ToArray();
+            var scores = scores2.ToArray();
+
+            // sort array results
+            Array.Sort(scores, keys);
+            Array.Reverse(keys);
+            Array.Reverse(scores);
+
+            //build final array
+            var result = new List<Trigram>();
+            for (int x = 0; x < keys.Length; x++)
+            {
+                result.Add(new Trigram(keys[x], scores[x]));
+            }
+
+            trigrams = result.ToArray();
+
+        }
+
+        public bool HasTrigram(string trigram)
+        {
+            foreach (Trigram t in trigrams)
+            {
+                if (t.Token == trigram) return true;
+            }
+            return false;
+        }
+
+        public int GetScore(string trigram)
+        {
+            foreach (Trigram t in trigrams)
+            {
+                if (t.Token == trigram) return t.Score;
+            }
+            throw new Exception("No score found for '" + trigram + "'");
+        }
+    }
+
+    public class Trigram
+    {
+        public string _token = null;
+        public int _score = 0;
+
+        public string Token
+        {
+            get { return _token; }
+            set { _token = value; }
+        }
+
+        public int Score
+        {
+            get { return _score; }
+            private set { _score = value; }
+        }
+
+        public Trigram()
+        {
+            _token = null;
+            _score = 0;
+        }
+        public Trigram(string t, int s)
+        {
+            Token = t;
+            Score = s;
+        }
+    }
+}
