ee5a0d6bd9401fdba8ffd00244f32df98a4bba24
jmbotero <jbotero@hotmail.com>
2020-09-18 19:08:57 -0400

    add solution for problem 20 final review pass

--- JuanMartin.Utilities/Euler/UtilityEulerProjectSolver.cs
+++ JuanMartin.Utilities/Euler/UtilityEulerProjectSolver.cs
@@ -38,7 +38,7 @@ namespace JuanMartin.Utilities.Euler
                 new Problem(17,NumberLetterCount,new long[] {1,1000}),
                 new Problem(18,MaximumPathSum,"75;95,64;17,47,82;18,35,87,10;20,04,82,47,65;19,01,23,75,03,34;88,02,77,73,07,63,67;99,65,04,28,06,16,70,92;41,41,26,56,83,40,80,70,33;41,48,72,33,47,32,37,16,94,29;53,71,44,65,25,43,91,52,97,51,14;70,11,33,28,77,73,17,78,39,68,17,57;91,71,52,38,17,14,91,43,58,50,27,29,48;63,66,04,68,89,53,67,30,73,16,69,87,40,31;04,62,98,27,23,09,70,98,73,93,38,53,60,04,23"),//"3;7,4;2,4,6;8,5,9,3"),
                 new Problem(19,CountingSundays,"01/01/1901;12/31/2000"),
-                new Problem(20,FactorialDigitSum,(ulong)100),
+                new Problem(20,FactorialDigitSum,100),
                 new Problem(21,AmicableNumbers,10000),
                 new Problem(22,NameScores,@"C:\Git\JuanMartin.ToolSet\JuanMartin.EulerProjectSolver\data\names.txt|,|"""),
                 new Problem(23,NonAbundantSum,28123),
@@ -855,9 +855,9 @@ namespace JuanMartin.Utilities.Euler
         /// <returns></returns>
         public static Result FactorialDigitSum(Problem arguments)
         {
-            var number = arguments.BigNumber;
-            var factorial = UtilityMath.FactorialOverFlow(number);
-            var sum = UtilityMath.DigitsSum(factorial);
+            var number = arguments.IntNumber;
+            var factorial = UtilityMath.FactorialLoop<BigInteger>(number);
+            var sum = UtilityMath.DigitsSum(factorial.ToString());
 
             var answer = sum.ToString();
             var message = string.Format("The sum of the digits of {0}! is {1}", number, sum);
