d40e164c8a14ad8e1e5f076ca35e47f2b9e3075f
jmbotero <jbotero@hotmail.com>
2021-02-06 20:51:10 -0500

    add solution for problem 89 third pass

--- JuanMartin.Utilities/Euler/RomanNumeral.cs
+++ JuanMartin.Utilities/Euler/RomanNumeral.cs
@@ -8,8 +8,9 @@ namespace JuanMartin.Utilities.Euler
 {
     public class RomanNumeral
     {
-        private readonly SortedDictionary<char, int> Denominations = new SortedDictionary<char, int>() { { 'I', 1 }, { 'V', 2 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+        private readonly SortedDictionary<char, int> Denominations = new SortedDictionary<char, int>() { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
         private string _numeral;
+        private SortedDictionary<string,int> _draft = new SortedDictionary<string, int>(); //contain first traslation evaluating substractions
         public string Value
         {
             get { return _numeral; }
@@ -22,55 +23,120 @@ namespace JuanMartin.Utilities.Euler
 
         public bool IsValid()
         {
+            int substraction_counter = 1;
+            string[] xcm_reductions = new string[]
+            {   new String('I',10),
+                new String('V', 2),
+                new String('I', 100),
+                new String('v', 20),
+                new String('X', 10),
+                new String('L', 2),
+                new String('I', 1000),
+                new String('V', 200),
+                new String('X', 100),
+                new String('L', 20),
+                new String('C', 10),
+                new String('D', 2)
+            };
+
             // check rules:
-            // 1. Numerals must be arranged in descending order of size.
-            // 2. M, C, and X cannot be equalled or exceeded by smaller denominations.
-            // 3. D, L, and V can each only appear once.
+            // rule_1. (valid_charachters): Roman numerals contain only I, V, X, L, C, D, M
+            // rule_2. (descending_numerals): Numerals must be arranged in descending order of size.
+            // rule_3. (mcx_reduction): M, C, and X cannot be equalled or exceeded by smaller denominations.
+            // rule_4. (dlv_once): D, L, and V can each only appear once.
             // from https://projecteuler.net/about=roman_numerals
-            
             var valid_charachters = Value.All(c => Denominations.ContainsKey(c));
-            var descending_numerals = true;
 
-            var keys = Denominations.Keys.ToList();
-            foreach(var c in Value)
+            if (valid_charachters)
             {
-                var current_index = keys.IndexOf(c);
-                char next_c = (current_index <= Value.Length - 1) ? keys[current_index + 1] : '\0';
+                bool mcx_reduction = !xcm_reductions.Any(v => Value.Contains(v));
 
-                // is a pair
-                if(next_c != '\0')
+                if (mcx_reduction)
                 {
-                    var value1 = Denominations[c];
-                    var value2 = Denominations[next_c];
+                    bool dlv_once = Value.Count(c => c == 'D') <= 1 && Value.Count(c => c == 'L') <= 1 && Value.Count(c => c == 'V') <= 1;
 
-                    // is subtractive
-                    if(value2 < value1)
+                    if (dlv_once)
                     {
-                        // 1. Only one I, X, and C can be used as the leading numeral in part of a subtractive pair.
-                        // 2. I can only be placed before V and X.
-                        // 3. X can only be placed before L and C.
-                        // 4. C can only be placed before D and M.
-                        if (!"IXC".Contains(c) || (c == 'I' && !"VX".Contains(next_c)) || (c == 'X' && !"LC".Contains(next_c)) || (c == 'C' && !"DM".Contains(next_c)))
+                        var descending_numerals = true;
+
+                        for (var i = 0; i < Value.Length; i += 2)
+                        {
+                            var c = Value[i];
+                            char next_c = (i == Value.Length - 1) ? '\0' : Value[i + 1];
+
+                            // is a pair
+                            if (next_c != '\0')
+                            {
+                                var value1 = Denominations[c];
+                                var value2 = Denominations[next_c];
+
+                                // is subtractive
+                                if (value1 < value2)
+                                {
+                                    // a. Only one I, X, and C can be used as the leading numeral in part of a subtractive pair.
+                                    // b. I can only be placed before V and X.
+                                    // c. X can only be placed before L and C.
+                                    // d. C can only be placed before D and M.
+                                    var rule_a = "IXC".Contains(c);
+                                    var rule_b = c == 'I' && "VX".Contains(next_c);
+                                    var rule_c = c == 'X' && "LC".Contains(next_c);
+                                    var rule_d = c == 'C' && "DM".Contains(next_c);
+                                    bool substraction_rules = (rule_a || rule_b || rule_c || !rule_d);
+
+                                    if (!substraction_rules)
+                                    {
+                                        descending_numerals = false;
+                                        break;
+                                    }
+
+                                    _draft.Add($"s{substraction_counter}", value2 - value1);
+                                    substraction_counter++;
+                                }
+                                else
+                                {
+                                    _draft.Add(c.ToString(), Denominations[c]);
+                                    _draft.Add(next_c.ToString(), Denominations[next_c]);
+                                }
+                            }
+                            else
+                            {
+                                _draft.Add(c.ToString(), Denominations[c]);
+                            }
+                        }
+
+                        if (_draft.Values.First() < _draft.Values.Last())
                         {
                             descending_numerals = false;
-                            break;
+                        }
+
+                        if (descending_numerals)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
                         }
                     }
-                    else if(value1 == value2 && !"IVDL".Contains(c))
+                    else
                     {
-                        descending_numerals = false;
-                        break;
+                        return false;
                     }
                 }
+                else
+                {
+                    return false;
+                }
             }
-            bool dlv_once = Value.Count(c => c == 'D') > 1 && Value.Count(c => c == 'L') > 1 && Value.Count(c => c == 'V') > 1;
-
-            return valid_charachters && descending_numerals && dlv_once; 
+            else
+            {
+                return false;
+            }
         }
 
         public int ToArabic()
         {
-            throw new NotImplementedException();
+            return (IsValid()) ? _draft.Values.Sum() : -1;
         }
     }
 }
